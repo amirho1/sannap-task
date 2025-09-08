80693145894e7f192ae0e5862a973c4a8914f990
Amirhossein Salighedar <amirhossein.s@arianalabs.io>
2025-09-08 22:59:43 +0330

    refactor(Header): remove unnecessary div for cleaner markup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,8 @@ import Logo from "./Logo";
 
 export default function Header() {
   return (
-    <header className="h-[207px] bg-primary p-4 rounded-b-2xl relative ">
-      <div className="">
+    <header className="h-[207px] bg-primary p-4 rounded-b-2xl relative">
         <Logo className="absolute top-4 left-1/2 -translate-x-1/2 z-10" />
-      </div>
     </header>
   );
 }
