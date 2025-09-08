f0060236847a4a659c2e03eaf3d0418f95787d9f
Amirhossein Salighedar <amirhossein.s@arianalabs.io>
2025-09-08 21:21:43 +0330

    feat(translations): add Persian translations for app localization

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import fa from "@/locales/fa.json";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function translate(key: keyof typeof fa) {
+  return fa[key] || key;
 }
