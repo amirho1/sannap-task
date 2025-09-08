9c3f0eb36a5fc0713aa3140daeede046d0f34b60
Amirhossein Salighedar <amirhossein.s@arianalabs.io>
2025-09-08 22:58:39 +0330

    fix(Header): add z-index to Logo for proper stacking context

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="h-[207px] bg-primary p-4 rounded-b-2xl relative ">
       <div className="">
-        <Logo className="absolute top-4 left-1/2 -translate-x-1/2 " />
+        <Logo className="absolute top-4 left-1/2 -translate-x-1/2 z-10" />
       </div>
     </header>
   );
