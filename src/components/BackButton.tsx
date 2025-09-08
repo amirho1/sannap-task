59d0afd45c345438e9d51b2102170d7fc17cf06d
Amirhossein Salighedar <amirhossein.s@arianalabs.io>
2025-09-08 21:23:33 +0330

    feat(BackButton): add BackButton component for navigation

---
+++ src/components/BackButton.tsx
@@ -0,0 +1,15 @@
+import { ChevronRight } from "lucide-react";
+import { Button } from "./ui/button";
+import type { HTMLAttributes } from "react";
+
+export default function BackButton(props: HTMLAttributes<HTMLButtonElement>) {
+  function handleOnBackClick() {
+    history.back();
+  }
+
+  return (
+    <Button variant="link" onClick={handleOnBackClick} {...props}>
+      <ChevronRight className="w-6 h-6" size={24} color="white" />
+    </Button>
+  );
+}
