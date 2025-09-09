ba57051aabdc46f2687fc61f4e96c71e12d582f4
Amirhossein Salighedar <amirhossein.s@arianalabs.io>
2025-09-09 10:48:02 +0330

    style: adjust layout height and format welcome message

--- src/components/routes/home/index.tsx
+++ src/components/routes/home/index.tsx
@@ -4,13 +4,15 @@ import { Link } from "react-router";
 
 export default function Home() {
   return (
-    <div className="h-[calc(100vh-97px)] bg-primary flex flex-col-reverse p-6 gap-6">
+    <div className="h-[calc(100vh-72px)] bg-primary flex flex-col-reverse p-6 gap-6">
       <Link to={"/register"} className="w-full">
         <Button variant="white" className="w-full">
           {translate("register")}
         </Button>
       </Link>
-      <p className="text-white text-center font-medium">{translate("welcomeToRegisterAgentClick")}</p>
+      <p className="text-white text-center font-medium">
+        {translate("welcomeToRegisterAgentClick")}
+      </p>
     </div>
   );
 }
