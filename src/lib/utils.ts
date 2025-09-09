d323c1e01a8733ffbb2322b2a3f755bd48f6b2e6
Amirhossein Salighedar <amirhossein.s@arianalabs.io>
2025-09-09 10:49:57 +0330

    feat: add apiRoutes and debounce utility function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,3 +9,18 @@ export function cn(...inputs: ClassValue[]) {
 export function translate(key: keyof typeof fa) {
   return fa[key] || key;
 }
+
+export const apiRoutes = {
+  createOTP: "/agent/verification/signup/create_otp/",
+  validateOTP: "/agent/verification/signup/validate_otp/",
+};
+
+export function debounce(func: (...args: any[]) => any, delay: number = 1000) {
+  let timeoutId: NodeJS.Timeout | null = null;
+
+  return (...args: any[]) => {
+    if (timeoutId) clearTimeout(timeoutId);
+
+    timeoutId = setTimeout(() => func(...args), delay);
+  };
+}
