9e18aa77bb27576d7dc664a54b268f01f935b3f7
Amirhossein Salighedar <amirhossein.s@arianalabs.io>
2025-09-09 12:10:47 +0330

    style: update error state styles for Input component

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -14,7 +14,7 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
         "rounded-md border border-muted-foreground bg-transparent px-4 py-3 text-base shadow-xs transition-[color,box-shadow]",
         "outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium",
         "disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        "data-[error=true]:focus-visible:border-destructive",
+        "data-[error=true]:border-destructive data-[error=true]:border-2",
         "focus-visible:border-primary-shade-02",
         "focus-visible:border-2",
         "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
