423a04f62d343119892067619dca0cfe39b3283e
Amirhossein Salighedar <amirhossein.s@arianalabs.io>
2025-09-10 13:38:32 +0330

    style(button): enhance font size of buttons

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -7,7 +7,7 @@ import { cn } from "@/lib/utils";
 const buttonVariants = cva(
   cn(
     "inline-flex items-center cursor-pointer justify-center gap-2 whitespace-nowrap rounded-md",
-    "text-base font-medium transition-all disabled:pointer-events-none disabled:opacity-50",
+    "text-base font-normal transition-all disabled:pointer-events-none disabled:opacity-50",
     "[&_svg]:pointer-events-none shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-primary",
     "focus-visible:primary/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 ",
     "dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive"
