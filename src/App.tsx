32f8ec7e8ddbde3ce4fd3d626c41fded31019307
Amirhossein Salighedar <amirhossein.s@arianalabs.io>
2025-09-10 14:17:18 +0330

    chore(App):  ad base url checking

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import { RouterProvider } from "react-router";
 import routes from "./components/routes";
+import { config } from "./lib/config";
 
 export default function App() {
+  // !Checking for env
+  if (!config.baseURL) console.error(`
+    ******************************************
+                BASE URL MISSING
+    ******************************************
+    `);
   return <RouterProvider router={routes} />;
 }
